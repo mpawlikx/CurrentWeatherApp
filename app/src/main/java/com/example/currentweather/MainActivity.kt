7ded02548cb9957453213c5473dbd44bedc2e8c0
maciek <sennymaciek@gmail.com>
2020-05-14 13:43:14 +0200

    poprawiono wyświetlanie

--- app/src/main/java/com/example/currentweather/MainActivity.kt
+++ app/src/main/java/com/example/currentweather/MainActivity.kt
@@ -1,27 +1,34 @@
 package com.example.currentweather
 
+import android.annotation.SuppressLint
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.currentweather.models.WeatherResponseModel
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    var currentWeather: WeatherResponseModel? = null
+    private var currentWeather: WeatherResponseModel? = null
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-                val sharedPref = getSharedPreferences("LOCATION", Context.MODE_PRIVATE)
+        val sharedPref = getSharedPreferences("LOCATION", Context.MODE_PRIVATE)
 
-        city_name_edit_text.text = sharedPref.getString("CITY_NAME", "")
+        city_name_edit_text.setText(sharedPref.getString("CITY_NAME", ""))
+        temperature_text.text = sharedPref.getString("TEMPERATURE", "temp")
+        pressure_text.text = sharedPref.getString("PRESSURE", "pres")
+        humidity_text.text = sharedPref.getString("HUMIDITY", "hum")
+        wind_text.text = sharedPref.getString("WIND", "wind")
+
+        //todo usunąc powtórzenia, dodać datę i naprawić bo wywala
 
         load_weather.setOnClickListener {
             val city_name = city_name_edit_text.text.toString().trim()
@@ -36,15 +43,21 @@ class MainActivity : AppCompatActivity() {
                 Log.d("HTTP_JSON", weatherJson)
                 currentWeather = Gson().fromJson(weatherJson, WeatherResponseModel::class.java)
 
-                var temperature = currentWeather.main.temp.toString().trim()
-                var pressure = currentWeather.main.pressure.toString().trim()
-                var humidity = currentWeather.main.humidity.toString().trim()
-                var wind = currentWeather.wind.speed.toString().trim()
+                val temperature = currentWeather!!.main.temp.toString().trim()
+                val pressure = currentWeather!!.main.pressure.toString().trim()
+                val humidity = currentWeather!!.main.humidity.toString().trim()
+                val wind = currentWeather!!.wind.speed.toString().trim()
 
-                temperature_text.text = "$temperature"
-                pressure_text.text = "$pressure hPa"
-                humidity_text.text="$humidity %"
-                wind_text.text="$wind km/h"
+                editor.putString("TEMPERATURE", temperature)
+                editor.putString("PRESSURE", pressure)
+                editor.putString("HUMIDITY", humidity)
+                editor.putString("WIND", wind)
+                editor.apply()
+
+                temperature_text.text = sharedPref.getString("TEMPERATURE", "temp")
+                pressure_text.text = sharedPref.getString("PRESSURE", "pres")
+                humidity_text.text = sharedPref.getString("HUMIDITY", "hum")
+                wind_text.text = sharedPref.getString("WIND", "wind")
 
             }
             job.start()
